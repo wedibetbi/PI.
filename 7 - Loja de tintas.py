bb9728caf3089dfebccf286db80e70d4e22dd92f
Kim Borguezan <sophiaborguezankim@gmail.com>
2023-09-25 00:50:13 -0300

    Delete 7 - Loja de tintas.py

--- 7 - Loja de tintas.py
+++
@@ -1,7 +0,0 @@
-import math
-
-area = float(input(""))
-latas = math.ceil(area / 12 / 18)
-preco = 80 * latas
-
-print("Para uma parede de área "+"{:.2f}".format(area)+", você vai precisar de "+str(latas)+" latas, com o custo de R$ "+"{:.2f}".format(preco)+".")
