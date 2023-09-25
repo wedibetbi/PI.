21bf6c9fd7fbba89f568ebc877f574a0aa7b0094
Kim Borguezan <sophiaborguezankim@gmail.com>
2023-09-25 20:32:53 -0300

    Add files via upload

---
+++ 1.4 - Antecessor e sucessor.py
@@ -0,0 +1,7 @@
+no = int(input(""))
+ant = no - 1
+suc = no + 1
+
+print("Você digitou o número "+str(no)+".")
+print("O antecessor de "+str(no)+" é "+str(ant)+".")
+print("O sucessor de "+str(no)+" é "+str(suc)+".")
