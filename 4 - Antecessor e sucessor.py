ccc48f7181f2de575e6fb997280b681c78bbbd70
Kim Borguezan <sophiaborguezankim@gmail.com>
2023-09-25 00:51:06 -0300

    Delete 4 - Antecessor e sucessor.py

--- 4 - Antecessor e sucessor.py
+++
@@ -1,7 +0,0 @@
-no = int(input(""))
-ant = no - 1
-suc = no + 1
-
-print("Você digitou o número "+str(no)+".")
-print("O antecessor de "+str(no)+" é "+str(ant)+".")
-print("O sucessor de "+str(no)+" é "+str(suc)+".")
