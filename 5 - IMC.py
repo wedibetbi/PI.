aac0720cff90839ef5887dc18682edcd15fe804d
Kim Borguezan <sophiaborguezankim@gmail.com>
2023-09-25 00:51:13 -0300

    Delete 5 - IMC.py

--- 5 - IMC.py
+++
@@ -1,4 +0,0 @@
-peso = float(input(""))
-altura = float(input(""))
-imc = peso / (altura ** 2)
-print("Para um peso de "+"{:.2f}".format(peso)+" e altura "+"{:.2f}".format(altura)+", o IMC calculado é "+"{:.2f}".format(imc)+".")
